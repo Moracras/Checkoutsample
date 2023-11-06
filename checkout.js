23808b4483e107ef9f3047b9612dc14fb3189910
Moracras <tavfikkarademir@gmail.com>
2023-11-06 23:51:51 +0300

    ste2

--- checkout.js
+++ checkout.js
@@ -2,18 +2,112 @@
 //*        CHECKOUT APP
 //***********************************/
 
-const deleteProducts = document.querySelector(".delete-div .fa-thrash-can")
+
+// CONSTANTS
+const SHIPPING_PRICE = 25.99
+const FREE_SHIPPING_LIMIT = 3000
+const TAX_RATE = 0.18
+
+
+// SELECTORS
+const deleteProducts = document.querySelector(".delete-div .fa-trash-can")
 const products = document.querySelector(".products")
 
 
 
-// DELETE PRODUCTS BUTTON EVENT
-
-deleteProducts.addEventListener("clcik", (e) => {
-    if(confirm("Are you sure you want to delete this?")){
-        products.textContent ="No product"
-        products.classList.add("no-product")
-        // document.querySelector(".delete-div").computedStyleMap.display = "none" // 1
-        e.target.parentElement.style.display = "none" //aynı özellik 2
-    }
+// EVENTS
+//? Delete Products button event
+deleteProducts.addEventListener("click", (e) => {
+   confirm("Silmek istedigine emin misiniz?") && noProductCheck()
+    
+  
 })
+
+products.addEventListener("click", (e) => {
+  console.log(e.target)
+
+  //   if (e.target.classList.contains("fa-minus")) {
+  //     alert("minus")
+  //   } else if (e.target.classList.contains("fa-plus")) {
+  //     alert("plus")
+  //   } else if (e.target.classList.contains("fa-trash-can")) {
+  //     alert("remove")
+  //   } else {
+  //     alert("diger")
+  //   }
+
+  if (e.target.classList.contains("fa-plus")) {
+    // document.getElementById("quantity").innerText++
+    e.target.previousElementSibling.innerText++
+    calculateProductPrice(e.target)
+  }else if (e.target.classList.contains("fa-minus")){
+    if(e.target.nextElementSibling.innerText > 1){
+    e.target.nextElementSibling.innerText--
+    calculateProductPrice(e.target)
+}
+  }else if (e.target.classList.contains("fa-trash-can")){
+    e.target.closest(".product").remove()
+    calculateProductPrice()
+    }
+  }
+)
+
+const calculateProductPrice = (btn) => {
+    const discountedPrice = btn
+      .closest(".product-info")
+      .querySelector("#discounted-price").textContent
+  
+    const quantity = btn
+      .closest(".buttons-div")
+      .querySelector("#quantity").textContent
+  
+    const productPrice = btn
+      .closest(".buttons-div")
+      .querySelector("#product-price")
+  
+    productPrice.textContent = (discountedPrice * quantity).toFixed(2)
+    calculateTotalPrice()
+  }
+
+const calculateTotalPrice = () =>{
+    const prices = document.querySelectorAll("#product-price")
+    
+
+    // selected product hesabı 
+    const subtotal = [...prices].reduce((sum, price) => sum + Number(price.textContent), 0
+    )
+
+    // shipping
+
+    const shippingPrice = subtotal >= FREE_SHIPPING_LIMIT || subtotal === 0 ? 0 : SHIPPING_PRICE
+
+    // tax hesabı 
+
+    const taxPrice = subtotal * TAX_RATE
+
+    // TOTAL value
+
+    const totalPrice = subtotal + shippingPrice + taxPrice
+
+    // hesplanan degerlerin doma basılması 
+    document.getElementById("selected-price").textContent = subtotal.toFixed(2)
+    document.getElementById("shipping").textContent =shippingPrice.toFixed(2)
+    document.getElementById("tax").textContent = taxPrice.toFixed(2)
+
+    document.getElementById("total").textContent = totalPrice.toFixed(2)
+
+    !totalPrice && noProductCheck()
+}
+
+const noProductCheck = () =>{
+    products.textContent = "No product"
+        products.classList.add("no-product")
+        document.querySelector (".delete-div").style.display = "none"
+
+}
+
+window.addEventListener("load", () =>{
+    calculateTotalPrice()
+
+})
+        
